8a63bd1f1dfd4b5b916512907b0d220b3141be62
Saif Projects <saif78763455@gmail.com>
2024-07-14 19:49:54 +0530

    some changes done

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -15,8 +15,9 @@ require('dotenv').config(); // Add this line to load environment variables
 const dbConnectionString = process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/mern_homepage";
 
 mongoose.connect(dbConnectionString, {
-  useNewUrlParser: true,
-  // useUnifiedTopology: true
+ useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000 // 30 seconds
 }).then(() => {
   console.log("Connection is successful !!");
 }).catch((e) => {
